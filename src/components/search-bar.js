b3e4142f9388f2fcac33e47711398c55e25794f9
abiduzz420 <abiduzair420@gmail.com>
2017-06-21 15:46:23 +0530

    onInputChange method added to SearchBar

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    };
+  }
   render() {
     return (
       <div>
-        <input />
+        <input
+          onChange={event => this.setState({ term: event.target.value })}
+        />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import SearchBar from './components/search-bar';
 
 const API_KEY = 'AIzaSyCvJSPJeCuyCC43fqxq8vlzkfS9e2g0iyM';
 
-const App = () => {
-  return <SearchBar />;
-};
+class App extends Component {
+  render() {
+    return <SearchBar />;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
